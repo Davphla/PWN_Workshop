948a89b98cdd8798f44597bb655ff655a4003b54
Davd <david.gozlan@epitech.eu>
2022-10-20 13:55:17 +0200

    :bug: Fixed ex01

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-ex01:
+all:  ex01 ex02 ex03 ex04
+
+
+ex01: ex01.c
 	gcc ex01.c -o ex01 -fno-stack-protector -no-pie
 
-ex02:
+ex02: ex02.c
 	gcc ex02.c -o ex02 -fno-stack-protector -no-pie
 
-ex03:
+ex03: ex03.c
 	gcc ex03.c -o ex03 -fno-stack-protector -no-pie
 
-ex04:
+ex04: ex04.c
 	gcc ex04.c -o ex04
--- ex01.c
+++ ex01.c
@@ -15,13 +15,13 @@
  * https://0xrick.github.io/binary-exploitation/bof1/
  */
 int main() {
+    char cmd[64] = "ls";
     char s[64];
-    char cmd[64] = "ls -l";
 
     puts("Hello ! Welcome to my world!");
     puts("Input your name, and I'll print you my favorite command !!");
     scanf("%s", &s);
-    printf("Your name is : \"%s\"\n", s);
+    printf("Your name is : \"%s\" and it's %lu size long\n", s, strlen(s));
     if (strcmp(cmd, "ls -l") != 0) {
         puts("You've changed my command !! How dare you !");
     }
